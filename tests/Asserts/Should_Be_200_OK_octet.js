d7f3e1926d529e51879a50ccf160ab849e1a1d6f
Gary Woodfine <lnb0l9dc@duck.com>
2025-02-06 10:30:08 +0000

    Update content-type assertions in tests
    Revised test assertions to validate the correct content-type values. Changed from "application/json" to "application/octet-stream" and "image/png" where appropriate to match expected response formats.

--- tests/Asserts/Should_Be_200_OK_octet.js
+++ tests/Asserts/Should_Be_200_OK_octet.js
@@ -2,7 +2,7 @@ client.test("Successful request with OK 200", () => {
     client.assert(response.status === 200, "HTTP Response status code is not 200");
 });
 
-client.test("Response content-type is json", () => {
+client.test("Response content-type is octet stream", () => {
     var contentType = response.contentType.mimeType;
     client.assert(contentType === "application/octet-stream", "Expected content-type 'application/octet-stream' but received '" + contentType + "'");
 });
--- tests/Asserts/Should_Be_200_OK_png.js
+++ tests/Asserts/Should_Be_200_OK_png.js
@@ -2,7 +2,7 @@ client.test("Successful request with OK 200", () => {
     client.assert(response.status === 200, "HTTP Response status code is not 200");
 });
 
-client.test("Response content-type is json", () => {
+client.test("Response content-type is image/png", () => {
     var contentType = response.contentType.mimeType;
     client.assert(contentType === "image/png", "Expected content-type 'image/png' but received '" + contentType + "'");
 });
